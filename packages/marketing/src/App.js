d1e0535d260b4cb9b6d9f42f3616cafa1d34830f
Basak <tanoybasak@kpmg.com>
2022-05-25 18:55:05 +0530

    class name

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, withRouter } from "react-router-dom";
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
-
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+})
 export default () => {
     return <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <Router>
                 <Route exact path='/' component={Landing} />
                 <Route exact path='/pricing' component={Pricing} />
